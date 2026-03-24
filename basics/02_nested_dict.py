090f4bfe6a1870a73fc8ba97b9d471a0167be070
muskan-chouhan <mushkanc430@gmail.com>
2026-03-24 15:47:07 +0530

    methods of dict

--- basics/02_nested_dict.py
+++ basics/02_nested_dict.py
@@ -11,11 +11,26 @@ print(student["subject"]['java'])
 
 # DICTIONARY METHODS
 
-#ALL KEY ACCESS
+#1 keys methods  =>  ALL KEY ACCESS    
 print(student.keys())
 #type casting 
 print(list(student.keys()))
-
-
 #total number  of keys
 print(len(student))
+
+#2 values methods  => =
+print(list(student.values()))
+
+
+#3 . items methods  => = pair form mai deta hai 
+pairs = list(student.items())
+print(pairs[0])
+
+#4 .get methods  =>
+print(student['name'])
+print(student.get('name'))
+
+#5 upadte method
+
+new_dict = student.update({"city" : "dewas"})
+print(new_dict)
