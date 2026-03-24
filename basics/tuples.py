d2b341c12a15f7cc4f88894cca63eb85da7f7528
muskan-chouhan <mushkanc430@gmail.com>
2026-03-24 14:55:59 +0530

    All about tuples

---
+++ basics/tuples.py
@@ -0,0 +1,26 @@
+#immutable chnge  nhi kr kste hai  no allowed
+tup = (22,33,45,44,46)
+tup1 = (2,)
+print(tup1)
+print(tup[0])
+
+#mindex   kis index pr hai number chk kr kste hai 
+tup3 = [2,3,4,4,4]
+print(tup3.index(2))
+
+#count  kitni baar aata hai number exit krta hai chekr ksktehai 
+
+print(tup3.count(4))
+
+
+#question 1
+grade = ("c","d","a","a","b","b","a")
+print(grade.count("a"))
+
+#q2
+
+grade1 = ["c","d","a","a","b","b","a"]
+# print(grade1.sort())  return none wrong way
+grade1.sort()
+print(grade1)
+
