e2589d9890e9453d5988521a6b6101f7d7500b8c
muskan-chouhan <mushkanc430@gmail.com>
2026-03-24 17:44:58 +0530

    questions based on set and dictsionary

--- basics/04_question_set_dict.py
+++ basics/04_question_set_dict.py
@@ -11,3 +11,29 @@ print(dic)
 
 subject = {"python","java", "c++","python", "javascript","java","python","java","c++","c"}
 print(len(subject))
+
+
+#question 3
+
+marks = {}
+
+x = int(input("enter physics marks :"))
+y = (input("enter chemestry marks :"))
+z = (input("enter maths marks :"))
+
+marks.update({"phy" : x,"chm" : y, "maths": z})
+
+print(marks)
+
+#question 4
+
+set = {4, "9.0", 8.8, }
+print(set)
+
+#second posible solution
+
+values = {
+    ('float',9.0),
+    ('int',3)
+}
+print(values)
