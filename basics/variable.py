41b1f15b876bf97c35488a21959977d6baafe903
muskan-chouhan <mushkanc430@gmail.com>
2026-03-24 14:21:13 +0530

    Added basics folder

---
+++ basics/variable.py
@@ -0,0 +1,18 @@
+name = "muskan"  #string
+age = 23         #intiger
+age2 = age
+print("My age is",age2)
+price = 24.4      #boolean
+print(name,age,price)
+print("My name is",name)
+
+
+age3 = 27
+old = False
+a = None
+#type of 
+print(type(name))
+print(type(age))
+print(type(age3))
+print(type(old))
+print(type(a))
