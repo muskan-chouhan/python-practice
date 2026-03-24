490397f07c6bfe7c44ea322e1c5899ad50ec6afc
muskan-chouhan <mushkanc430@gmail.com>
2026-03-24 14:34:09 +0530

    type converstion and type casting

---
+++ basics/typeconverstion.py
@@ -0,0 +1,21 @@
+#typeconverstion  >automatically perfrom hota hai 
+a = 4
+b = 4.4
+sum = a + b
+print(sum)
+
+#type casting > forcefully covert krwana
+x = int("4")
+y = 4.4
+add = x + y
+print(add)
+
+#input
+name  = int(input("enter your number"))
+print(type(name),name)
+
+
+val1 = int(input("enter number"))
+val2 = int(input("enter number"))
+sum = val1 + val2
+print(sum)
