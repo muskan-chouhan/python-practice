eccd6f543ad49e11c91e8a8e94b46dd953d138ae
muskan-chouhan <mushkanc430@gmail.com>
2026-03-24 17:31:04 +0530

    questions based on set and dictsionary

---
+++ basics/04_question_set_dict.py
@@ -0,0 +1,13 @@
+#question 1
+dic = {
+    "table" : ["a piece of furniture","list of fact & figures"],
+    "cat"  : "a samll animal"
+}
+
+print(dic)
+
+
+#question 2  >set
+
+subject = {"python","java", "c++","python", "javascript","java","python","java","c++","c"}
+print(len(subject))
