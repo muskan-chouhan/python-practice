7660468a785dc9ec12f3b6cfb7b1f3f801e0c169
muskan-chouhan <mushkanc430@gmail.com>
2026-03-24 15:16:24 +0530

    how to create dictionary to store key value pair

---
+++ basics/01_dictionary.py
@@ -0,0 +1,10 @@
+info = {
+    "name" : "Nikita",
+    "cgpa" : 88.23,
+    "learning" : "Web Devoping",
+    "is_adult" : True,
+    "subject" : ['python',"PHP", "Wordpess"],        #list
+    "topics" : ("dict" , "set")                      #tuple 
+}
+print(info)
+
