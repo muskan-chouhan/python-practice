cdc2b337c0f55b0e3be69fb14bcd27c59cce5e7c
muskan-chouhan <mushkanc430@gmail.com>
2026-03-25 21:57:47 +0530

    practice questions

---
+++ 01_basics/04_question_set_dict.py
@@ -0,0 +1,40 @@
+#question 1
+dic = {
+    "table" : ["a piece of furniture","list of fact & figures"],
+    "cat"  : "a samll animal"
+}
+
+print(dic)
+
+
+#question 2  >set
+
+subject = {"python","java", "c++","python", "javascript","java","python","java","c++","c"}
+print(len(subject))
+
+
+#question 3
+
+marks = {}
+
+x = int(input("enter physics marks :"))
+y = (input("enter chemestry marks :"))
+z = (input("enter maths marks :"))
+
+marks.update({"phy" : x,"chm" : y, "maths": z})
+
+print(marks)
+
+#question 4
+
+set = {4, "9.0", 8.8, }
+print(set)
+
+#second posible solution
+
+values = {
+    ('float',9.0),
+    ('int',3)
+}
+
+print(values)
