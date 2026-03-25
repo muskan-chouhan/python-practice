fc14d2992ac23808874661832173b0d9c0f5fdb8
muskan-chouhan <mushkanc430@gmail.com>
2026-03-25 21:50:32 +0530

    all about operations

---
+++ 01_basics/operation.py
@@ -0,0 +1,40 @@
+x= 2
+y = 3
+sum = x + y
+diff = x - y
+div = x/y  #always floating value dega 
+module = x % y  #reminder 
+power = x**y    #power
+print(sum,diff,div,module,power)
+
+#comparision operator
+a = 50
+b = 40
+
+print(a == b) # output  > false
+print(a != b) 
+print(a >= b) 
+print(a > b) 
+print(a <= b) 
+
+
+#add sum value in variable
+num = 10
+#num = num + 10
+num += 10
+print(num)
+
+
+#logical oprator
+print(not False) #always give true  >not oprator always give opposite value
+print(not True)  #always give false
+
+#AND oprator > both value are true then output true otherwise give false
+val1 = True
+val2 = True
+print("AND oprator:",val1 and val2)
+
+
+#or oprator  >  one statement true then give true  
+
+print("OR oprator:",a == b or a > b)
