16b47e44a5d3c7bb6e0af0713fb137389228d83f
muskan-chouhan <mushkanc430@gmail.com>
2026-03-25 21:54:16 +0530

    all about dictionary

---
+++ 01_basics/01_dictionary.py
@@ -0,0 +1,34 @@
+info = {
+    "name" : "Nikita",
+    "cgpa" : 88.23,
+    "learning" : "Web Devoping",
+    "is_adult" : True,
+    "subject" : ['python',"PHP", "Wordpess"],        #list
+    "topics" : ("dict" , "set"),                     #tuple
+     12.0 : "23.3"                                            
+}
+print(info)
+
+
+#access value
+
+print(info["name"])
+print(info["learning"])
+print(info["subject"])
+print(info["topics"])
+print(info["cgpa"])
+
+
+#asign new values
+info['name'] = "anya"   #overwrite
+print(info)
+
+#add new key and value
+info['surname'] = "sharma"
+print(info)
+
+#empty dict create
+null_dict = {}
+null_dict['name'] = "kashish chouhan"
+print(null_dict)
+
