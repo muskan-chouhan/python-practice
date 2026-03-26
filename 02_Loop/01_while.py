bd2b4663acfaa4d8aa673edf41dd9aa4816bf913
muskan-chouhan <mushkanc430@gmail.com>
2026-03-26 12:32:58 +0530

    add loop folder

---
+++ 02_Loop/01_while.py
@@ -0,0 +1,30 @@
+# while  condition :   jb tk true hota hai
+#     #some work
+
+count = 1
+while count <= 5 :
+    print("hello")
+    count += 1
+print(count)
+
+i = 1
+while i <= 5 :
+    print("muskan")
+    i += 1
+print(i)
+
+#print number  from 1 to 5
+
+a = 1
+while a <= 5 :
+    print(a)
+    a += 1
+print("Loop ended") 
+
+#reverse print
+
+a = 5
+while a >= 1 :
+    print(a)
+    a -= 1
+print("Loop ended") 
