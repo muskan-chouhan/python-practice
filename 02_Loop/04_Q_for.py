544b114f0bc27b2fd853ae84c44fed747bdc1130
muskan-chouhan <mushkanc430@gmail.com>
2026-03-26 12:46:54 +0530

    practice question forloop

---
+++ 02_Loop/04_Q_for.py
@@ -0,0 +1,12 @@
+#q
+
+list = [1,4,6,9,16,18,24,26,29]
+for val in list:
+    print(val)
+print("end")
+#q2
+x = 222
+num = (11,44,66,99,111,222,444,222)
+for val in num:
+    if(val == x):
+        print(val)
