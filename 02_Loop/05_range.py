b018a26424a60ff1526d18edcee044cf33ba8c2e
muskan-chouhan <mushkanc430@gmail.com>
2026-03-26 12:48:00 +0530

    all about range

---
+++ 02_Loop/05_range.py
@@ -0,0 +1,16 @@
+#start = 0 default ,step > kitne se increase krna hai default 1 se  , stop kaha tk krna hai 
+seq = range(5)
+# print(seq[0])
+# print(seq[1])
+# print(seq[2])
+# print(seq[3])
+# print(seq[4])
+for i in seq:   #for i in range(5)
+    print(i)
+
+for y in range(2,10,2):          #start and stop    and step 
+    print(y)
+print("complete")
+#q print even num
+for i in range(2,101,2):
+    print(i,"even number")    
