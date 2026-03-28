1e2b1fe5fcef058cd621a573d9cf738e5cb52a08
muskan-chouhan <mushkanc430@gmail.com>
2026-03-28 14:16:49 +0530

    reverse array

---
+++ TCS/01_array/04_reverseArr.py
@@ -0,0 +1,10 @@
+#Reverse a given Array
+#using reverse method = orignal list change hogi 
+arr1 = ["z","y","x"]
+arr1.reverse()
+print(arr1)
+
+# using slicing 
+arr = [9,8,7,6,5]
+print(arr[::-1])
+
