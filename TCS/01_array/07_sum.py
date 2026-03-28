8c99b092de601953c0131d4f986308f609e6da81
muskan-chouhan <mushkanc430@gmail.com>
2026-03-28 14:20:30 +0530

    Calculate sum of the elements of the array

---
+++ TCS/01_array/07_sum.py
@@ -0,0 +1,16 @@
+#Calculate Sum of the Elements of the Array
+
+arr = [2,3,4,5,6,7,8]
+summ = 0
+for el in arr:
+    summ += el 
+print(summ)
+
+
+arr1 =[33,44,55]
+
+def cal (arr):
+    return sum(arr)
+
+total_sum = cal(arr1)
+print(total_sum)
