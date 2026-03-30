5a05ef93b1608f740c2e01986e871cdcd230f44b
muskan-chouhan <mushkanc430@gmail.com>
2026-03-30 15:25:17 +0530

    Find all repeating elements in an array

---
+++ TCS/01_array/13_repeating.py
@@ -0,0 +1,15 @@
+#Find all repeating elements in an array
+arr = [1,4,2,1,2,4]
+def findRepeating(arr):
+    n = len(arr)
+
+    for i in range(n):
+        for j in range(i+1, n):
+            if arr[i] == arr[j]:
+                print(arr[i])
+                break   # same element baar-baar print na ho
+
+
+# Test
+arr = [1,2,3,2,4,1,5]
+findRepeating(arr)
